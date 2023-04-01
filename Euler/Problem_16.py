aac28e91e48a6a7ecae3b54409ef1047740992f9
Ryan <ryan.john.christensen12@gmail.com>
2023-04-01 10:43:32 -0400

    removing euler subfolder since it's its own repo now

--- Euler/Problem_16.py
+++
@@ -1,9 +0,0 @@
-# Problem 16 Power Digit Sum
-x = 2**1000
-print(x)
-value = str(2**1000)
-totalling = []
-for i in range(len(value)):
-	total = int(value[i])
-	totalling.append(total)
-print(sum(totalling))
--- Euler/README.md
+++
@@ -1,2 +0,0 @@
-# Python Solutions for Project Euler Problems!
-
